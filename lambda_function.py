1a9478382af6c6296f7ee3a6993cbe06b54fbff0
Armin Samii <armin.samii@gmail.com>
2020-08-31 21:51:53 -0400

    Add event/context

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ import time
 import gpt3_light as gpt3
 import runner
 
-def lambda_handler():
+def lambda_handler(event, context):
   babyMonitor = babytrump.TwitterBabyMonitor()
   runner.run_now(babyMonitor)
